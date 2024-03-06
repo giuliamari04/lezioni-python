4534dc992e751c7997f0bb728f5385d403c7164f
giuliamari04 <giuliamari04@gmail.com>
2024-03-06 18:16:38 +0100

    type casting

---
+++ typeCasting.py
@@ -0,0 +1,16 @@
+x = 1   #int
+y = 2.0 #float
+z = "3" #string
+
+x = float(x)
+x = str(x)
+
+y = int(y)
+y = str(y)
+
+z = str(z)
+
+#print prende solo valori di tipo string , ecco a cosa serve type casting
+print(x)
+print(y)
+print(z*3)
