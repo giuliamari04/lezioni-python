8221c98d896d09e826d6a1ed61e299a90729d4f7
giuliamari04 <giuliamari04@gmail.com>
2024-03-06 18:07:51 +0100

    caratteristiche stringhe

---
+++ strings.py
@@ -0,0 +1,31 @@
+name = "Bro"
+# age = 21
+# acctractive = True
+
+#lungezza stringa
+print(len(name))
+
+#posizione indice lettera
+print(name.find("o"))
+
+#iniziale maiuscola
+print(name.capitalize())
+
+#tutto maiuscolo tutto minuscolo
+print(name.upper())
+print(name.lower())
+
+#True o False se è un numero (vero)
+print(name.isdigit())
+
+#True o false se è una lettera dell'alfabeto
+print(name.isalpha())
+
+#conta quanti elemnti ci sono con quella caratteristica
+print(name.count("o"))
+
+#sostituisce lettera con seconda lettera
+print(name.replace("o","a"))
+
+#moltiplica stringa
+print(name*3)
