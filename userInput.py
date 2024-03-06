31d59b2706ddac63ec93c248d97a6f87d90d2d33
giuliamari04 <giuliamari04@gmail.com>
2024-03-06 18:24:28 +0100

    user input2

--- userInput.py
+++ userInput.py
@@ -1,7 +1,8 @@
 name = input("What's your name?: ")
 age = int(input("How old are you?: "))
 height = float(input("How tall are you?: "))
-age = age + 1
+
+#age = age + 1
 print("Hello "+name+" nice to meet you")
 print("you are "+str(age)+" years old")
 print("you are "+ str(height)+" cm")
