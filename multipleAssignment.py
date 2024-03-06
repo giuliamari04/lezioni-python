5dec2f8077decf111b549dece7c8e2829b34e47b
giuliamari04 <giuliamari04@gmail.com>
2024-03-06 18:07:35 +0100

    multiple assignment

---
+++ multipleAssignment.py
@@ -0,0 +1,12 @@
+# name = "Bro"
+# age = 21
+# acctractive = True
+
+name, age, actractive = "Bro",21,True
+
+Spongebob = Patrik = Sandy = 30
+
+print(Spongebob)
+print(Sandy)
+print("name:"+name,", age: "+str(age),", attractive:"+str(actractive))
+
