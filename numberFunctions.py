06dfe018a8bbef7502ce21be68d7b1d556d87466
giuliamari04 <giuliamari04@gmail.com>
2024-03-06 18:32:47 +0100

    numbers function

---
+++ numberFunctions.py
@@ -0,0 +1,28 @@
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+#stampa numero intero
+print(round(pi))
+
+#arrotondato per eccesso
+print(math.ceil(pi))
+
+#arrotondato per difetto
+print(math.floor(pi))
+
+#numero assoluto
+print(abs(pi))
+
+#esponenziale
+print(pow(pi,2))
+
+#radice
+print(math.sqrt(420))
+
+#valire massimo e minimo
+print(max(x,y,z))
+print(min(x,y,z))
