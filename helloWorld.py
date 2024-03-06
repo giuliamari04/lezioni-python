b767a11644e99c42a4ab121e9d9e9f3a157740e8
giuliamari04 <giuliamari04@gmail.com>
2024-03-06 17:50:50 +0100

    first commit

---
+++ helloWorld.py
@@ -0,0 +1,16 @@
+print("Hello world")
+print("it's me")
+
+name = "Bro"
+surname = "Man"
+age = 21
+age += 1
+height = 160.75
+isHuman = True
+full_name = name+" "+surname
+print("hello "+full_name)
+print ("your age: "+str(age))
+print ("your height is: "+str(height)+"cm")
+print ("you're human: "+str(isHuman))
+#print (type(name))
+
